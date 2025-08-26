79979ba27fb82dae97119bf4d86a679a05f95df1
yersi alviarez <yersialviarez@gmail.com>
2025-08-26 11:45:18 -0500

    creacion de ruoyes/characters.js... para introduccion de nuevos personajes

--- starwars_backend/index.js
+++ starwars_backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Ruta raiz de prueba
-app.get('/', (req, res) => {7
+app.get('/', (req, res) => {
     res.send('API de Star Wars lista');
 });
 
@@ -35,6 +35,69 @@ app.get('/characters/:id', async (req, res) => {
     } catch(error) {
         console.error(error);
         res.status(500).json({ error: "Error obteniendo personaje" });
+    }
+});
+
+//Crear un nuevo personaje
+app.post('/characters', async (req, res) => {
+    try {
+        const { name, gender, birth_year, image_url, movies, origin_planet } = req.body;
+
+        const newCharacter = await Character.create({
+            name, 
+            gender,
+            birth_year,
+            image_url,
+            movies,
+            origin_planet
+        });
+
+        res.status(201).json(newCharacter);
+    }
+});
+
+// Actualizar un personaje por ID
+app.put('/characters/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, gender, birth_year, image_url, movies, origin_planet } = req.body;
+
+        const character = await Character.findByPk(id);
+        if (!character) {
+            return res.status(404).json({ error: "personaje no encontrado" });
+        }
+
+        await character.update({
+            name, 
+            gender,
+            birth_year,
+            image_url,
+            movies,
+            origin_planet
+        });
+
+        res.json(character);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Error actualizando personaje"});
+        }
+});
+
+//Eliminar un personaje por ID
+app.delete('/characters/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const character = await Character.findByPk(id);
+        if(!character) {
+            return res.status(404).json({ error: "personaje no encontrado" });
+        }
+
+        await character.destroy();
+        res.json({ message: "personaje eliminado correctamente" });
+    } catch(error) {
+            console.error(error);
+            res.status(500).json({ error: "error eliminando personaje" });
     }
 });
 
