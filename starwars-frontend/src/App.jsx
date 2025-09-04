595be89cee53281376ae0b0e34d2dd959ff6133b
yersi alviarez <yersialviarez@gmail.com>
2025-09-04 16:49:38 -0500

    subida de imagen para cada personaje/ eliminacion de personajes irrelevantes/adision de personajes relevantes fuera de la base de datos

--- starwars-frontend/src/App.jsx
+++ starwars-frontend/src/App.jsx
@@ -10,28 +10,35 @@ function App() {
     .catch(error => console.error(error));
   }, []);
 
-  return(
-    <>
-    <div style={{display: "flex", justifyContent: "center", width: "100vw", flexDirection: "column", border: "1px solid black"}}>
-      <h1 style={{justifyContent: "center", border: "1px solid black", margin: "20px auto" }}>Star Wars Characters</h1>
+  return (
+    <div className="min-h-screen px-6 py-10">
+      <h1 className="text-3xl font-bold text-yellow-400 text-center mb-8">
+        Star Wars Characters
+      </h1>
 
-      {characters.map((c) => (
-      <div className='bg-gray-900 rounded-xl shadow-lg p-4 text-center text-white hover:scale-105 transition-transform'>
-          <img 
-            src={`/images/${c.id}.jpg`} 
-            alt={c.name}
-            className='w-32 h-64 object-cover rounded-lg mb-4'
-            onError={(e) => (e.target.src = "/images/placeolder.jpg")} 
-          />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+        {characters.map((c) => (
+          <div
+            key={c.id}
+            className="bg-gray-900 rounded-2xl shadow-lg p-5 text-center text-white hover:scale-105 transition-transform"
+          >
+            {/* Contenedor de imagen con tamaño fijo */}
+            <div className="w-32 h-32 mx-auto rounded-full overflow-hidden bg-black">
+              <img
+                src={`/images/${c.id}.jpg`}
+                alt={c.name}
+                className="w-full h-full object-cover"
+                onError={(e) => (e.currentTarget.src = "/images/placeolder.jpg")}
+              />
+            </div>
 
-      <h3 className='text-xl font-bold text-yellow-400'>{c.name}</h3>
-      <p className='text-sm'>{c.origin_planet || "Unknow" }</p>
-      <p className='text-sm text-gray-400'>{c.birth_year}</p>
-
+            <h3 className="text-xl font-bold text-yellow-400 mt-3">{c.name}</h3>
+            <p className="text-sm">{c.origin_planet || "Unknown"}</p>
+            <p className="text-sm text-gray-400">{c.birth_year}</p>
+          </div>
+        ))}
       </div>
-      ))}
     </div>
-    </>
   );
 }
 
