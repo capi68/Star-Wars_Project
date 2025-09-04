02beb25d27156c2e5699aeac3733dcdba1a5e8a9
yersi alviarez <yersialviarez@gmail.com>
2025-09-03 21:48:08 -0500

    realizacion de tarjetas estilos basicos

--- starwars-frontend/src/App.jsx
+++ starwars-frontend/src/App.jsx
@@ -14,13 +14,22 @@ function App() {
     <>
     <div style={{display: "flex", justifyContent: "center", width: "100vw", flexDirection: "column", border: "1px solid black"}}>
       <h1 style={{justifyContent: "center", border: "1px solid black", margin: "20px auto" }}>Star Wars Characters</h1>
-      <ul>
-        {characters.map(c => (
-          <li key={c.id}>
-            <strong>{c.name}</strong> ({c.origin_planet || "Unknown" })
-          </li>
-        ))}
-      </ul>
+
+      {characters.map((c) => (
+      <div className='bg-gray-900 rounded-xl shadow-lg p-4 text-center text-white hover:scale-105 transition-transform'>
+          <img 
+            src={`/images/${c.id}.jpg`} 
+            alt={c.name}
+            className='w-32 h-64 object-cover rounded-lg mb-4'
+            onError={(e) => (e.target.src = "/images/placeolder.jpg")} 
+          />
+
+      <h3 className='text-xl font-bold text-yellow-400'>{c.name}</h3>
+      <p className='text-sm'>{c.origin_planet || "Unknow" }</p>
+      <p className='text-sm text-gray-400'>{c.birth_year}</p>
+
+      </div>
+      ))}
     </div>
     </>
   );
