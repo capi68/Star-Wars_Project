4050360346d68046e4a3083b90ad395111804f60
yersi alviarez <yersialviarez@gmail.com>
2025-09-04 21:48:28 -0500

    realizacion de input buscador de personajes por nombre, planeta o pelicula... Actualizacion de base de datos hasta ID 50

--- starwars-frontend/src/App.jsx
+++ starwars-frontend/src/App.jsx
@@ -3,12 +3,25 @@ import axios from "axios";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3000/characters")
+    axios
+    .get("http://localhost:3000/characters")
     .then(res => setCharacters(res.data))
     .catch(error => console.error(error));
   }, []);
+
+  // Filtro por busqueda
+  const filteredCharacters = characters.filter((c) => {
+    const term = search.toLocaleLowerCase();
+    return (
+      c.name.toLocaleLowerCase().includes(term) || 
+      (c.origin_planet && c.origin_planet.toLocaleLowerCase().includes(term)) || 
+      (c.movies && c.movies.toLocaleLowerCase().includes(term))
+    );    
+  });
+
 
   return (
     <div className="min-h-screen px-6 py-10">
@@ -16,8 +29,16 @@ function App() {
         Star Wars Characters
       </h1>
 
+      { /* Input de busqueda*/ }
+      <input 
+        type="text"
+        placeholder='Buscar por nombre, planeta o pelicula'
+        value={search}
+        onChange={(e) => { setSearch(e.target.value)}}
+        className='mb-6 p-2  border boder-gray-400 rounded-md w-80 text-black'
+      />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
-        {characters.map((c) => (
+        {filteredCharacters.map((c) => (
           <div
             key={c.id}
             className="bg-gray-900 rounded-2xl shadow-lg p-5 text-center text-white hover:scale-105 transition-transform"
