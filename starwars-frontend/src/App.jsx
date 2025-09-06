2ecfc879592d9b4f75e8c285eebf1d57383b9541
yersi alviarez <yersialviarez@gmail.com>
2025-09-05 20:15:18 -0500

    finalizacion del proyecto con fondo de pagina, ultimos estilos aplicados en responsive y Modal

--- starwars-frontend/src/App.jsx
+++ starwars-frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
   const [characters, setCharacters] = useState([]);
   const [search, setSearch] = useState("");
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
+  // calls all characters in the database
   useEffect(() => {
     axios
     .get("http://localhost:3000/characters")
@@ -12,7 +15,8 @@ function App() {
     .catch(error => console.error(error));
   }, []);
 
-  // Filtro por busqueda
+
+  // function to search for character by attribute
   const filteredCharacters = characters.filter((c) => {
     const term = search.toLocaleLowerCase();
     return (
@@ -23,27 +27,42 @@ function App() {
   });
 
 
+
   return (
-    <div className="min-h-screen px-6 py-10">
-      <h1 className="text-3xl font-bold text-yellow-400 text-center mb-8">
+    <>
+
+    {/* Background Image */}
+    <img src="/images/Background_Image.jpeg" alt="Background image" className='fixed inset-0 w-full h-full object-cover -z-10' />
+
+
+    {/* Div Container */ }
+    <div className="relative z-10 flex flex-col justify-center items-center min-h-screen px-6 py-10">
+      <h1 className="text-4xl md:text-5xl 2xl:text-6xl font-bold text-yellow-400 text-center mb-8">
         Star Wars Characters
       </h1>
 
-      { /* Input de busqueda*/ }
+      { /* Search Input */ }
       <input 
         type="text"
         placeholder='Buscar por nombre, planeta o pelicula'
         value={search}
         onChange={(e) => { setSearch(e.target.value)}}
-        className='mb-6 p-2  border boder-gray-400 rounded-md w-80 text-black'
+        className='mb-6 p-2  border border-gray-400 rounded-md w-80 text-black'
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+
+      {/* Grid Characters cards */}
+
+      <div className="grid place-items-center grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl  mx-auto">
         {filteredCharacters.map((c) => (
+
+          //card generated for each character
           <div
             key={c.id}
-            className="bg-gray-900 rounded-2xl shadow-lg p-5 text-center text-white hover:scale-105 transition-transform"
-          >
-            {/* Contenedor de imagen con tamaño fijo */}
+            className="w-48 bg-gray-800 rounded-2xl shadow-lg p-5 text-center text-white hover:scale-105 transition-transform cursor-pointer "
+            onClick={() => setSelectedCharacter(c)}
+            >
+
+            {/* Image container*/}
             <div className="w-32 h-32 mx-auto rounded-full overflow-hidden bg-black">
               <img
                 src={`/images/${c.id}.jpg`}
@@ -54,12 +73,61 @@ function App() {
             </div>
 
             <h3 className="text-xl font-bold text-yellow-400 mt-3">{c.name}</h3>
-            <p className="text-sm">{c.origin_planet || "Unknown"}</p>
-            <p className="text-sm text-gray-400">{c.birth_year}</p>
           </div>
         ))}
       </div>
     </div>
+
+    { /* modal */}
+    <AnimatePresence>
+    {selectedCharacter && (
+      <motion.div className='fixed inset-0 bg-black/70 flex items-center justify-center z-50'
+           onClick={() => setSelectedCharacter(null)}
+           initial={{ opacity: 0 }}
+           animate={{ opacity: 1 }}
+           exit={{ opacity: 0 }}
+           transition={{ duration: 0.4 }}
+      >
+        <motion.div className='bg-gray-900 rounded-2xl shadow-xl p-6 w-96 text-white relative transform transition-all'
+             onClick={(e) => e.stopPropagation()} //Prevents it from closing if we click inside the card
+             initial={{ scale: 0.6, opacity: 0 }}
+             animate={{ scale: 1, opacity: 1 }}
+             exit={{ scale: 0.6, opacity: 0 }}
+             transition={{
+              type: "spring",
+              stiffness: 200,
+              damping: 20,
+              duration: 0.5
+             }}
+        >
+          { /* Button "X" */ }
+          
+          <button className='absolute top-2 right-2 text-gray-400 hover:text-white text-xl'
+                  onClick={() => setSelectedCharacter(null)}
+          > 
+            X
+          </button>
+
+          {/* Character Image */}
+            <div className="w-32 h-32 mx-auto bg-black rounded-full overflow-hidden">
+              <img
+                src={`/images/${selectedCharacter.id}.jpg`}
+                alt={selectedCharacter.name}
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+             {/* Character Info */}
+            <h2 className="text-2xl font-bold text-yellow-400 mt-4">{selectedCharacter.name}</h2>
+            <p><strong>Gender:</strong> {selectedCharacter.gender}</p>
+            <p><strong>Birth Year:</strong> {selectedCharacter.birth_year}</p>
+            <p><strong>Planet:</strong> {selectedCharacter.origin_planet}</p>
+            <p><strong>Movies:</strong> {selectedCharacter.movies}</p>
+          </motion.div>
+        </motion.div>
+    )}
+    </AnimatePresence>
+    </>
   );
 }
 
